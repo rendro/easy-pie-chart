0c66313493645eed81322fb08bf450a45084295c
Robert Fleischmann <rendro87@gmail.com>
2013-12-01 10:45:48 +0100

    add test for overwritable options

--- test/unit/jquery.js
+++ test/unit/jquery.js
@@ -1,34 +1,60 @@
 describe('Unit testing jQuery version of easy pie chart', function() {
 	var $el;
 
-	var options = {
-		size: 200
-	};
+	var createInstance = function(options, el) {
+		options = options || {};
+		el = el || '<span class="chart"></span>';
+		return function() {
+			$el = $(el);
+			$('body').append($el);
+			$el.easyPieChart(options);
+		};
+	}
 
-	beforeEach(function(){
-		$el = $('<span class="chart"></span>');
-		$('body').append($el);
+	describe('initialize plugin', function() {
+		beforeEach(createInstance());
 
-		$el.easyPieChart(options);
+		it('should insert a canvas element', function() {
+			expect($el.html()).toContain('canvas');
+		});
 	});
 
-	it('inserts a canvas element', function() {
-		expect($el.html()).toContain('canvas');
-	});
 
 	describe('takes size option and', function() {
 		var $canvas;
+		beforeEach(createInstance({
+			size: 200
+		}));
 
 		beforeEach(function() {
 			$canvas = $el.find('canvas');
 		});
 
 		it('set correct width', function() {
-			expect($canvas.width()).toBe(options.size);
+			expect($canvas.width()).toBe(200);
 		});
 
 		it('set correct height', function() {
-			expect($canvas.height()).toBe(options.size);
+			expect($canvas.height()).toBe(200);
+		});
+	});
+
+	describe('options should be overwritable by data attributes', function() {
+		var $canvas;
+		beforeEach(createInstance({
+			size: 200
+		}, '<span class="chart" data-size="400"></span>'));
+
+		beforeEach(function() {
+			$canvas = $el.find('canvas');
+		});
+
+		it('overwrite width', function() {
+			expect($canvas.width()).toBe(400);
+		});
+
+		it('overwrite height', function() {
+			expect($canvas.height()).toBe(400);
 		});
 	});
 
