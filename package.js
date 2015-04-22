eab0f9a515b898c109fffa87740383381bc377a2
Dotan Simha <dotansimha@gmail.com>
2015-04-22 19:00:14 +0300

    Update version

--- package.js
+++ package.js
@@ -3,18 +3,17 @@ Package.describe({
 	version: '2.1.6',
 	summary: 'Meteor package for jQuery easyPieChart plugin!',
 	git: 'https://github.com/dotansimha/easy-pie-chart.git',
-	documentation: 'Readme.md'
+	documentation: null
 });
 
 Package.onUse(function (api) {
 	api.versionsFrom('METEOR@0.9.0.1');
     api.use('jquery', 'client');
 
-	api.add_files(['jquery.easypiechart.js'], 'client');
+	api.add_files(['dist/jquery.easypiechart.js'], 'client');
 });
 
 Package.onTest(function (api) {
 	api.use('tinytest');
 	api.use('rendro:easy-pie-chart');
-	api.addFiles('easy-pie-chart-tests.js');
 });
